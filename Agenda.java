e5eb5d25a44e0de5ac5009aac4be6456884c438a
Edgar Alvarez <edgarfabero85@gmail.com>
2018-04-19 20:28:03 +0200

    creado un metodo que devuelve todos los nombres de nuestra agenda

--- Agenda.java
+++ Agenda.java
@@ -13,13 +13,13 @@ public class Agenda
         agenda = new HashMap<>();
     }
 
+    // introduce el nombre de la persona y su numero en la agenda 
     
     public void enterNumber(String name, String number)
     {
         agenda.put(name,number);
     }
-    
-    
+    // busca el numero de la persona en la agenda 
     
     public String lookupNumber(String name){
         String resultado = "";
@@ -27,5 +27,12 @@ public class Agenda
         resultado = agenda.get(name);
         
         return resultado;
+    }
+    
+    //imprime todos los nombres de la agenda
+    
+    public void printAllNames(){
+        System.out.println(agenda.keySet());
+        
     }
 }
